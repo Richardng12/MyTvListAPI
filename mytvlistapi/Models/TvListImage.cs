b560e8d82e1ca654bc918c509c338e9a0d63b977
Richard Ng <Richardng1998@gmail.com>
2018-11-22 01:43:34 +1300

    Change of author

--- mytvlistapi/Models/TvListImage.cs
+++ mytvlistapi/Models/TvListImage.cs
@@ -11,7 +11,6 @@ namespace mytvlistapi.Models
         public string Title { get; set; }
         public string Tags { get; set; }
         public string Score { get; set; }
-        public string Author { get; set; }
         public IFormFile Image { get; set; }
     }
 }
