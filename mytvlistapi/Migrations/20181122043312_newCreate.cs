cd0135c38268f8bb8517d3c7fdd3e91f05a4319b
Richard Ng <Richardng1998@gmail.com>
2018-11-22 17:34:20 +1300

    Add comments

---
+++ mytvlistapi/Migrations/20181122043312_newCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mytvlistapi.Migrations
+{
+    public partial class newCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "TvItem",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Comments",
+                table: "TvItem",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "TvItem");
+
+            migrationBuilder.DropColumn(
+                name: "Comments",
+                table: "TvItem");
+        }
+    }
+}
