e3f681696f5bc01dd2b09eccc7db577249dc6890
Richard Ng <Richardng1998@gmail.com>
2018-11-23 21:17:58 +1300

    Applicatoin insights

--- mytvlistapi/Program.cs
+++ mytvlistapi/Program.cs
@@ -41,6 +41,7 @@ namespace mytvlistapi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
